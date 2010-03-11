7c02bb1c5c68f4820ecaef50db95eb9df3b4fdb7
Nug B00T <nugb00t@gmail.com>
2010-03-11 21:01:17 +0200

    + Resources - basic logic, WIP

--- engine/resources/resources.cpp
+++ engine/resources/resources.cpp
@@ -8,17 +8,74 @@ using namespace engine;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-bool Resources::initialize() {
-	TiXmlDocument doc("game.xml");
-	doc.LoadFile();
+bool Resources::update() {
+	kaynine::AutoLock<> lock(guard_);
 
+	// go through the resource_ array and schedule file read if there's a vacant place in the queue
+	for (uint slot = 0, item = 0; slot < MAX_LOADS && item < MAX_RESOURCES; ++slot) {
+		if (loads_[slot].status != Load::VACANT)
+			continue;
+		
+		while (resources_[item].status != Resource::PENDING && item < MAX_RESOURCES)
+			++item;
+		
+		load(resources_[item], loads_[slot]);
+	}
+	
 	return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-bool Resources::update() {
-	return true;
+uint Resources::add(const TCHAR* const path, void** const bufferPtr, bool** const statusPtr) {
+	kaynine::AutoLock<> lock(guard_);
+
+	assert(vacantResource_ < MAX_RESOURCES - 1 && path && bufferPtr && statusPtr);
+	
+	_tcsncpy(&resources_[vacantResource_].path[0], path, MAX_PATH);
+	resources_[vacantResource_].bufferPtr = bufferPtr;
+	resources_[vacantResource_].statusPtr = statusPtr;
+	
+	return vacantResource_++;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Resources::reset() {
+	for (uint i = 0; i < MAX_RESOURCES; ++i) {
+		resources_[i].path[0] = NULL;
+		resources_[i].bufferPtr = NULL;
+		resources_[i].statusPtr = NULL;
+		resources_[i].status = Resource::VACANT;
+	}
+
+	for (uint i = 0; i < MAX_LOADS; ++i)
+		loads_[i].status = Load::VACANT;
+	
+	vacantResource_ = 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Resources::load(const Resource& item, Load& slot) {
+	// open the file
+	HANDLE file = ::CreateFile(item.path, 
+							   GENERIC_READ, 
+							   FILE_SHARE_READ, 
+							   NULL, 
+							   OPEN_EXISTING, 
+							   FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED | FILE_FLAG_SEQUENTIAL_SCAN,
+							   NULL);
+	assert(file);
+	
+	uint size = ::GetFileSize(file, NULL);
+	
+	// set up overlapped struct
+	memset(&slot.overlapped, 0, sizeof(slot.overlapped));
+	slot.overlapped.hEvent = &slot;
+	
+	// initiate asio read
+	CHECKED_WINAPI_CALL(::ReadFileEx(file, NULL, size, &slot.overlapped, NULL));
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- engine/resources/resources.h
+++ engine/resources/resources.h
@@ -5,19 +5,53 @@ namespace engine {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Resources : public kaynine::PulseThreadObject {
-	static const uint PERIOD = 16;
-	static const uint DELAY = 4 * 10;
+	static const uint PERIOD = 100;
 
+	static const uint MAX_RESOURCES = 64;
+	static const uint MAX_LOADS = 4;
+	
+	struct Resource {
+		TCHAR path[MAX_PATH];
+		void** bufferPtr;
+		bool** statusPtr;
+
+		enum STATUS {
+			VACANT,
+			PENDING,
+			DONE
+		} status;
+	};
+	
+	struct Load {
+		OVERLAPPED overlapped;
+		
+		enum STATUS {
+			VACANT,
+			PROCESSING
+		} status;
+	};
+	
 public:
-	// interface: kaynine::PulseThreadObject
-	virtual const uint period() const { return PERIOD; }	// *1 milliseconds
-	virtual const uint delay() const { return DELAY; }		// *100 nanoseconds
-
-	virtual bool initialize();
+	// interface: kaynine::ThreadObject
+	virtual bool initialize() { reset(); return true; }
 	virtual bool update();
+
+	virtual const uint period() const { return PERIOD; }	// *1 milliseconds
+	virtual const uint delay() const { return 0; }			// *100 nanoseconds
+
+	// interface: own
+	uint add(const TCHAR* const path, void** const bufferPtr, bool** const statusPtr);
+	void reset();
 	
 private:
+	void load(const Resource& item, Load& slot);
 	
+private:
+	Resource resources_[MAX_RESOURCES];
+	uint vacantResource_;
+	Load loads_[MAX_LOADS];
+
+	kaynine::CriticalSection guard_;
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
